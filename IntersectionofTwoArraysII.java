2d29428a7fa52a2efeb5a4f96434041112eeed84
Chaitanya7725 <chaitanya7725@gmail.com>
2025-02-26 18:03:09 -0500

    Adding Intersection of 2 array with different approaches

--- IntersectionofTwoArraysII.java
+++ IntersectionofTwoArraysII.java
@@ -2,11 +2,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+// Using Pointers
+// TC: O(n log n) + O(m log m)
+// SC: O(n)
+
+// Using Binary search for locating the intersecting element
+// TC: O(m log n)
+// SC: O(n)
 public class IntersectionofTwoArraysII {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(intersect(new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 }))); // [2,2]
         System.out.println(Arrays.toString(intersect(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 }))); // [4,9]
+
+        System.out.println(Arrays.toString(intersectUsingBS(new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 }))); // [2,2]
+        System.out.println(Arrays.toString(intersectUsingBS(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 }))); // [4,9]
     }
 
     public static int[] intersect(int[] nums1, int[] nums2) {
@@ -37,6 +47,51 @@ public class IntersectionofTwoArraysII {
             result[i] = list.get(i);
         }
         return result;
+    }
+
+    private static int[] intersectUsingBS(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0)
+            return new int[] {};
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m > n) {
+            return intersectUsingBS(nums2, nums1);
+        }
+        int low = 0;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            int returning = binarySearch(nums2, low, n - 1, nums1[i]);
+            if (returning != -1) {
+                list.add(nums1[i]);
+                low = returning + 1;
+            }
+        }
+
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+
+    }
+
+    private static int binarySearch(int[] nums, int low, int high, int target) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                if (low == mid || nums[mid] != target) {
+                    return mid;
+                }
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
     }
 
 }
